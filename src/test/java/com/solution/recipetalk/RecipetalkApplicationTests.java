b54ba0886d8868c3eda03e3ac231505037c47db6
KimHyunJin <khj745700@naver.com>
2023-01-02 12:19:56 +0900

    init : 프로젝트 최초 시작 with 의존성 설정
    - spring data JPA
    - spring security
    - spring validation
    - spring starter web
    
    - h2 database
    - mysqldb
    
    - lombok
    
    - Oauth2 client
    - Oauth2 resource server

---
+++ src/test/java/com/solution/recipetalk/RecipetalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solution.recipetalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipetalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
