2a233e5963ccf970dd933d6f535c25a590b6a9b9
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2023-01-27 22:59:40 +0900

    add : RestAuthenticationEntryPoint, CustomFilterException TestController는 테스트용, 곧 삭제할 예정 RestAuthenticationEntryPoint : SecurityFilter 내 특정 필터에 대한 Excetpion 처리 CustomFilterException : AuthenticationException 구현체

--- src/main/java/com/solution/recipetalk/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/solution/recipetalk/config/jwt/JwtAuthorizationFilter.java
@@ -3,6 +3,7 @@ package com.solution.recipetalk.config.jwt;
 import com.solution.recipetalk.config.jwt.token.RequestToken;
 import com.solution.recipetalk.config.jwt.token.properties.AccessTokenProperties;
 import com.solution.recipetalk.config.jwt.token.properties.CommonTokenProperties;
+import com.solution.recipetalk.config.jwt.token.properties.RefreshTokenProperties;
 import com.solution.recipetalk.domain.user.login.repository.UserLoginRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -19,9 +21,14 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     @Autowired
     private UserLoginRepository userRepository;
 
-    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserLoginRepository userRepository) {
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserLoginRepository userRepository, RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
         super(authenticationManager);
         this.userRepository = userRepository;
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
     }
 
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager, AuthenticationEntryPoint authenticationEntryPoint) {
@@ -32,15 +39,21 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 
 
-        String jwtHeader = request.getHeader(AccessTokenProperties.HEADER_STRING);
+        String jwtAccessTokenHeader = request.getHeader(AccessTokenProperties.HEADER_STRING);
+        String jwtRefreshTokenHeader = request.getHeader(RefreshTokenProperties.HEADER_STRING);
 
-        if (jwtHeader == null || !jwtHeader.startsWith(CommonTokenProperties.TOKEN_PREFIX)){
-            chain.doFilter(request, response);
-            return;
-        }
+//        if (jwtAccessTokenHeader == null || !jwtAccessTokenHeader.startsWith(CommonTokenProperties.TOKEN_PREFIX)){
+//            chain.doFilter(request, response);
+//            return;
+//        }
 
         RequestToken requestToken = new RequestToken(request);
-        chain.doFilter(request, response);
+        try{
+            requestToken.getUsername();
+            chain.doFilter(request, response);
+        }catch (AuthenticationException e){
+            restAuthenticationEntryPoint.commence(request, response, e);
+        }
 
     }
 }
--- src/main/java/com/solution/recipetalk/config/jwt/token/RequestToken.java
+++ src/main/java/com/solution/recipetalk/config/jwt/token/RequestToken.java
@@ -2,8 +2,11 @@ package com.solution.recipetalk.config.jwt.token;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.solution.recipetalk.config.jwt.CustomFilterException;
 import com.solution.recipetalk.config.jwt.token.properties.AccessTokenProperties;
 import com.solution.recipetalk.config.jwt.token.properties.CommonTokenProperties;
+import com.solution.recipetalk.config.jwt.token.properties.RefreshTokenProperties;
+import com.solution.recipetalk.exception.common.NotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,19 +17,27 @@ import java.util.Optional;
 @Setter
 public class RequestToken {
     private String accessToken;
+    private String refreshToken;
     public RequestToken(HttpServletRequest request){
         this.accessToken = request.getHeader(AccessTokenProperties.HEADER_STRING).replace(CommonTokenProperties.TOKEN_PREFIX, "");
+        this.refreshToken = request.getHeader(RefreshTokenProperties.HEADER_STRING).replace(CommonTokenProperties.TOKEN_PREFIX, "");
+
+        System.out.println(this.accessToken);
+        System.out.println(this.refreshToken);
     }
 
-    public Optional<String> getTokenElement(String token, String element){
-        return Optional.ofNullable(JWT
-                .require(Algorithm.HMAC512(CommonTokenProperties.SECRET))
-                .build().verify(token)
-                .getClaim(element).asString());
-
+    public String getTokenElement(String token, String element){
+        try{
+            return Optional.ofNullable(JWT
+                    .require(Algorithm.HMAC512(CommonTokenProperties.SECRET))
+                    .build().verify(token)
+                    .getClaim(element).asString()).orElseThrow();
+        }catch (Exception e){
+            throw new CustomFilterException("Occur Exception");
+        }
     }
 
-    public Optional<String> getUsername(){
+    public String getUsername(){
         return getTokenElement(accessToken, "username");
     }
 }
