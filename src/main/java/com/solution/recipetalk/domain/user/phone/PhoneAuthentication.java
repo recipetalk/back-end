43d3b46576f3868d878c6e794d0b5c7a1e794186
KimHyunJin <khj745700@naver.com>
2023-01-28 21:30:59 +0900

    feat : PhoneAuthentication entity 생성
    - count는 default 0, isAuthentication = false 로 줌.

---
+++ src/main/java/com/solution/recipetalk/domain/user/phone/PhoneAuthentication.java
@@ -0,0 +1,37 @@
+package com.solution.recipetalk.domain.user.phone;
+
+import com.solution.recipetalk.domain.common.AuditingEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table
+public class PhoneAuthentication extends AuditingEntity {
+    @Id
+    @Column(name = "phone_num", nullable = false)
+    @Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})")
+    private String phoneNum;
+
+    @Column(name = "is_authentication", columnDefinition = "boolean default false")
+    private Boolean isAuthentication;
+
+    @Column(name = "count", columnDefinition = "TINYINT default 0")
+    private Integer count;
+
+    @Column(name = "auth_num")
+    private String authNum;
+
+    @PrePersist
+    protected void prePersist(){
+        isAuthentication = false;
+        count = 0;
+    }
+
+}
