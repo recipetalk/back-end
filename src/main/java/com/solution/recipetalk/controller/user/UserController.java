1347952fa5d90790c385248f08038d8012d1bec7
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2023-01-28 22:02:02 +0900

    feat : 회원가입 테스트 플로우도 작성해두었습니다. 추후에 삭제하시면 되고 getUsername쪽을 인터페이스로 빼서 관리하셔도 됩니다.

--- src/main/java/com/solution/recipetalk/controller/user/UserController.java
+++ src/main/java/com/solution/recipetalk/controller/user/UserController.java
@@ -1,15 +1,19 @@
 package com.solution.recipetalk.controller.user;
 
 
+import com.solution.recipetalk.config.jwt.JwtTokenHeader;
+import com.solution.recipetalk.config.jwt.token.RequestToken;
+import com.solution.recipetalk.config.jwt.token.properties.AccessTokenProperties;
+import com.solution.recipetalk.dto.user.SignUpUserReqDto;
 import com.solution.recipetalk.service.user.FindUserService;
+import com.solution.recipetalk.service.user.RegisterUserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,8 +24,32 @@ public class UserController {
     @Autowired
     private final FindUserService findUserService;
 
+    @Autowired
+    private final RegisterUserService registerUserService;
+
     @GetMapping("/signup/{id}")
     public ResponseEntity<?> duplicatedUserCheck(@PathVariable("id")String userName) {
         return findUserService.findDuplicatedUsernameInUserLogin(userName);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUpUser(@RequestBody SignUpUserReqDto signUpUserReqDto){
+        return registerUserService.addUser(signUpUserReqDto);
+    }
+
+    @GetMapping("/need/auth")
+    public ResponseEntity<?> needAuthTest(HttpServletRequest request){
+        String username = getUsername(request);
+        return new ResponseEntity<>("권한 인증 완료 (" + username + ")", HttpStatus.OK);
+    }
+
+    private String getUsername(HttpServletRequest request){
+        JwtTokenHeader jwtAccessTokenHeader = new JwtTokenHeader(AccessTokenProperties.HEADER_STRING, request);
+        try{
+            RequestToken requestToken = new RequestToken(jwtAccessTokenHeader);
+            return requestToken.getElementInToken(requestToken.getToken(), "username");
+        }catch (Exception e){
+            throw new RuntimeException("권한없음");
+        }
     }
 }
