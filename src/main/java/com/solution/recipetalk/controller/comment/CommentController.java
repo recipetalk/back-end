878bb1e8f706872631f87c8a55664117db11759f
ghdwlgns <atene1408@gmail.com>
2023-02-01 03:43:44 +0900

    feat : 댓글 삭제 기능 구현
    삭제 기능
    - 특정 댓글 삭제(댓글 작성자, 관리자 가능)
    - 게시물에 등록된 댓글 전체 삭제(관리자만 가능)
    - 전체 댓글 삭제(관리자만 가능)

--- src/main/java/com/solution/recipetalk/controller/comment/CommentController.java
+++ src/main/java/com/solution/recipetalk/controller/comment/CommentController.java
@@ -1,8 +1,11 @@
 package com.solution.recipetalk.controller.comment;
 
 import com.solution.recipetalk.dto.comment.CommentCreateDTO;
+import com.solution.recipetalk.dto.comment.CommentModifyDTO;
+import com.solution.recipetalk.service.comment.EditCommentService;
 import com.solution.recipetalk.service.comment.FindCommentService;
 import com.solution.recipetalk.service.comment.RegisterCommentService;
+import com.solution.recipetalk.service.comment.RemoveCommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,12 @@ public class CommentController {
 
     @Autowired
     private final FindCommentService findCommentService;
+
+    @Autowired
+    private final EditCommentService editCommentService;
+
+    @Autowired
+    private final RemoveCommentService removeCommentService;
 
     @PostMapping("/{boardId}/comment")
     public ResponseEntity<?> commentAdd(
@@ -35,5 +44,33 @@ public class CommentController {
     @GetMapping("/user/comment")
     public ResponseEntity<?> commentListByLoginUser() {
         return findCommentService.findCommentsByUser();
+    }
+
+    @PatchMapping("/board/{boardId}/comment/{commentId}")
+    public ResponseEntity<?> commentModify(
+            @PathVariable Long boardId,
+            @PathVariable Long commentId,
+            @Valid @RequestBody String description
+    ) {
+        CommentModifyDTO commentModifyDTO = CommentModifyDTO.builder().description(description).build();
+        return editCommentService.modifyCommentById(boardId, commentId, commentModifyDTO);
+    }
+
+    @DeleteMapping("/board/{boardId}/comment/{commentId}")
+    public ResponseEntity<?> commentRemoveByBoardIdAndCommentId(
+            @PathVariable Long boardId,
+            @PathVariable Long commentId
+    ) {
+        return removeCommentService.removeCommentByIdAndBoardId(boardId, commentId);
+    }
+
+    @DeleteMapping("/board/{boardId}/comment")
+    public ResponseEntity<?> commentRemoveByBoardId(@PathVariable Long boardId) {
+        return removeCommentService.removeAllCommentsOfBoard(boardId);
+    }
+
+    @DeleteMapping("/comment")
+    public ResponseEntity<?> commentRemove() {
+        return removeCommentService.removeAllComments();
     }
 }
