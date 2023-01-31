c6395353788076785c247baa645275522d5529e4
ghdwlgns <atene1408@gmail.com>
2023-02-01 00:16:29 +0900

    feat : 댓글 조회 기능 추가.
    현재 로그인한 유저가 작성한 댓글 조회 : 댓글 / 대댓글 구별 없이 작성자명, 댓글 내용, 수정일 응답으로 보냄
    게시판에 등록된 댓글 조회 : 댓글 / 대댓글 구별 위한 부모 댓글 id, 작성자명, 댓글 내용, 수정일 응답으로 보냄

---
+++ src/main/java/com/solution/recipetalk/service/comment/impl/FindCommentServiceImpl.java
@@ -0,0 +1,55 @@
+package com.solution.recipetalk.service.comment.impl;
+
+import com.solution.recipetalk.domain.board.entity.Board;
+import com.solution.recipetalk.domain.board.repository.BoardRepository;
+import com.solution.recipetalk.domain.comment.entity.Comment;
+import com.solution.recipetalk.domain.comment.repository.CommentRepository;
+import com.solution.recipetalk.domain.user.entity.UserDetail;
+import com.solution.recipetalk.dto.comment.CommentInBoardResponseDTO;
+import com.solution.recipetalk.dto.comment.CommentResponseDTO;
+import com.solution.recipetalk.exception.board.CannotFindBoardException;
+import com.solution.recipetalk.service.comment.FindCommentService;
+import com.solution.recipetalk.util.ContextHolder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FindCommentServiceImpl implements FindCommentService {
+
+    @Autowired
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    private final BoardRepository boardRepository;
+
+    @Override
+    public ResponseEntity<?> findAllCommentsOfBoard(Long boardId) {
+        Board board = boardRepository.findById(boardId).orElseThrow(
+                CannotFindBoardException::new
+        );
+
+        List<Comment> allByBoard = commentRepository.findAllByBoard(board);
+
+        List<CommentInBoardResponseDTO> responseDTOS = allByBoard.stream().map(CommentInBoardResponseDTO::toResponse).toList();
+
+        return ResponseEntity.ok(responseDTOS);
+    }
+
+    @Override
+    public ResponseEntity<?> findCommentsByUser() {
+        UserDetail writer = ContextHolder.getUserDetail();
+
+        List<Comment> allByWriter = commentRepository.findAllByWriter(writer);
+
+        List<CommentResponseDTO> responseDTOS = allByWriter.stream().map(CommentResponseDTO::toResponse).toList();
+
+        return ResponseEntity.ok(responseDTOS);
+    }
+}
