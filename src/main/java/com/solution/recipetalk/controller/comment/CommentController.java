ebce3e32539ae643afbd3e784145e9b798ee3eb8
ghdwlgns <atene1408@gmail.com>
2023-02-01 00:16:29 +0900

    feat : 댓글 조회 기능 추가.
    현재 로그인한 유저가 작성한 댓글 조회 : 댓글 / 대댓글 구별 없이 작성자명, 댓글 내용, 수정일 응답으로 보냄
    게시판에 등록된 댓글 조회 : 댓글 / 대댓글 구별 위한 부모 댓글 id, 작성자명, 댓글 내용, 수정일 응답으로 보냄

--- src/main/java/com/solution/recipetalk/controller/comment/CommentController.java
+++ src/main/java/com/solution/recipetalk/controller/comment/CommentController.java
@@ -1,25 +1,39 @@
 package com.solution.recipetalk.controller.comment;
 
-import com.solution.recipetalk.dto.comment.CommentDTO;
+import com.solution.recipetalk.dto.comment.CommentCreateDTO;
+import com.solution.recipetalk.service.comment.FindCommentService;
 import com.solution.recipetalk.service.comment.RegisterCommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/{boardId}/comment")
+@RequestMapping("/")
 public class CommentController {
+    @Autowired
     private final RegisterCommentService registerCommentService;
 
-    @PostMapping("/")
+    @Autowired
+    private final FindCommentService findCommentService;
+
+    @PostMapping("/{boardId}/comment")
     public ResponseEntity<?> commentAdd(
             @PathVariable Long boardId
-            , @Valid @RequestBody CommentDTO.Create request
+            , @Valid @RequestBody CommentCreateDTO request
     ) {
         return registerCommentService.addComment(boardId, request);
     }
 
+    @GetMapping("/{boardId}/comment")
+    public ResponseEntity<?> commentListByBoard(@PathVariable Long boardId) {
+        return findCommentService.findAllCommentsOfBoard(boardId);
+    }
 
+    @GetMapping("/user/comment")
+    public ResponseEntity<?> commentListByLoginUser() {
+        return findCommentService.findCommentsByUser();
+    }
 }
--- src/main/java/com/solution/recipetalk/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/solution/recipetalk/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.solution.recipetalk.domain.comment.repository;
 
+import com.solution.recipetalk.domain.board.entity.Board;
 import com.solution.recipetalk.domain.comment.entity.Comment;
+import com.solution.recipetalk.domain.user.entity.UserDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByBoard(Board board);
+    List<Comment> findAllByWriter(UserDetail writer);
 }
