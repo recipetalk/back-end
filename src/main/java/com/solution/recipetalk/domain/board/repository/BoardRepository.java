3c0ff1ed7f456687149e6b23b73ea05db35ee139
ghdwlgns <atene1408@gmail.com>
2023-02-01 16:43:01 +0900

    add : test 위한 조회 기능 추가
    게시물 제목으로 조회하는 기능 임시로 추가. 추후 삭제 예정

--- src/main/java/com/solution/recipetalk/domain/board/repository/BoardRepository.java
+++ src/main/java/com/solution/recipetalk/domain/board/repository/BoardRepository.java
@@ -4,4 +4,5 @@ import com.solution.recipetalk.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    Board findBoardByTitle(String title);
 }
