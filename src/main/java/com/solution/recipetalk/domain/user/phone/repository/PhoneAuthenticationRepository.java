2a5dcee102b4db6fd11bc53600c382b8f502b55f
KimHyunJin <khj745700@naver.com>
2023-02-06 00:08:25 +0900

    revise : 휴대폰 인증 레포지토리 위치 변경

--- src/main/java/com/solution/recipetalk/domain/user/phone/repository/PhoneAuthenticationRepository.java
+++ src/main/java/com/solution/recipetalk/domain/user/phone/repository/PhoneAuthenticationRepository.java
@@ -1,6 +1,6 @@
-package com.solution.recipetalk.domain.user.repository;
+package com.solution.recipetalk.domain.user.phone.repository;
 
-import com.solution.recipetalk.domain.user.phone.PhoneAuthentication;
+import com.solution.recipetalk.domain.user.phone.entity.PhoneAuthentication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhoneAuthenticationRepository extends JpaRepository<PhoneAuthentication, String> {
