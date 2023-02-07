f3fd8658daabb5ead5a5d73c82f24475128a722b
KimHyunJin <khj745700@naver.com>
2023-02-07 22:47:30 +0900

    chore : RecipeRow 한번에 저장하기 위한 List 프로퍼티 추가

--- src/main/java/com/solution/recipetalk/dto/recipe/row/RecipeRowRegisterDTO.java
+++ src/main/java/com/solution/recipetalk/dto/recipe/row/RecipeRowRegisterDTO.java
@@ -2,6 +2,7 @@ package com.solution.recipetalk.dto.recipe.row;
 
 import com.solution.recipetalk.domain.recipe.entity.Recipe;
 import com.solution.recipetalk.domain.recipe.row.entity.RecipeRow;
+import jakarta.validation.Valid;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,19 +16,35 @@ import java.util.List;
 public class RecipeRowRegisterDTO {
     @NonNull
     private Long recipeId;
-    @NonNull
-    private String description;
 
-    //TODO : File valid 기능 필요
-    private List<MultipartFile> imgs;
-    @NonNull
-    private Long timer;
+    @Valid
+    private List<RecipeRowInfoDTO> recipeRowInfos;
 
-    public RecipeRow toRecipeRowEntity(Recipe recipe) {
-        return RecipeRow.builder()
-                .recipe(recipe)
-                .description(description)
-                .timer(timer)
-                .build();
+    public List<RecipeRow> toRecipeRowList(Recipe recipe) {
+        return recipeRowInfos.stream().map((recipeRowDescription) -> recipeRowDescription.toRecipeRowEntity(recipe)).toList();
     }
+
+    @AllArgsConstructor
+    @Getter
+    @NoArgsConstructor
+    @Builder
+    public static class RecipeRowInfoDTO {
+        @NonNull
+        private String description;
+
+        //TODO : File valid 기능 필요
+        private List<MultipartFile> imgs;
+        @NonNull
+        private Long timer;
+
+        public RecipeRow toRecipeRowEntity(Recipe recipe) {
+            return RecipeRow.builder()
+                    .recipe(recipe)
+                    .description(description)
+                    .timer(timer)
+                    .build();
+        }
+    }
+
+
 }
