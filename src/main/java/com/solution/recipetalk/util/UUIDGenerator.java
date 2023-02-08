c8bc46fe521fe3d7970710973041e1deaf9e3659
KimHyunJin <khj745700@naver.com>
2023-02-08 14:53:46 +0900

    add : s3 파일 업로더 기능 추가
    - UUID를 활용한 이미지 저장

---
+++ src/main/java/com/solution/recipetalk/util/UUIDGenerator.java
@@ -0,0 +1,10 @@
+package com.solution.recipetalk.util;
+
+import java.util.UUID;
+
+public class UUIDGenerator {
+
+    public static String getUUID(){
+        return UUID.randomUUID().toString();
+    }
+}
