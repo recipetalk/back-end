850d35e6b3d2926261f82dc01ac19b39378b50f3
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2023-03-17 00:10:57 +0900

    add: transactional readonly

--- src/main/java/com/solution/recipetalk/service/ingredient/trimming/impl/FindIngredientTrimmingServiceImpl.java
+++ src/main/java/com/solution/recipetalk/service/ingredient/trimming/impl/FindIngredientTrimmingServiceImpl.java
@@ -22,10 +22,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class FindIngredientTrimmingServiceImpl implements FindIngredientTrimmingService {
 
