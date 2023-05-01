8a783e85db5d72723727a81d845f766f9a930da3
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2023-05-01 17:16:27 +0900

    fix: 레시피 삭제 로직 수정

--- src/main/java/com/solution/recipetalk/service/recipe/impl/RemoveRecipeServiceImpl.java
+++ src/main/java/com/solution/recipetalk/service/recipe/impl/RemoveRecipeServiceImpl.java
@@ -1,16 +1,25 @@
 package com.solution.recipetalk.service.recipe.impl;
 
 import com.solution.recipetalk.domain.board.repository.BoardRepository;
+import com.solution.recipetalk.domain.image.repository.ImageRepository;
 import com.solution.recipetalk.domain.recipe.entity.Recipe;
+import com.solution.recipetalk.domain.recipe.ingredient.entity.RecipeIngredient;
+import com.solution.recipetalk.domain.recipe.ingredient.repository.RecipeIngredientRepository;
 import com.solution.recipetalk.domain.recipe.repository.RecipeRepository;
+import com.solution.recipetalk.domain.recipe.row.entity.RecipeRow;
+import com.solution.recipetalk.domain.recipe.row.img.entity.RecipeRowImg;
+import com.solution.recipetalk.domain.recipe.row.img.repository.RecipeRowImgRepository;
+import com.solution.recipetalk.domain.recipe.row.repository.RecipeRowRepository;
 import com.solution.recipetalk.exception.recipe.RecipeNotFoundException;
 import com.solution.recipetalk.service.recipe.RemoveRecipeService;
+import com.solution.recipetalk.service.recipe.row.RemoveRecipeRowService;
 import com.solution.recipetalk.util.ContextHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -19,6 +28,9 @@ import java.util.Objects;
 public class RemoveRecipeServiceImpl implements RemoveRecipeService {
     private final RecipeRepository recipeRepository;
     private final BoardRepository boardRepository;
+    private final RecipeRowRepository recipeRowRepository;
+    private final RecipeIngredientRepository recipeIngredientRepository;
+    private final RemoveRecipeRowService removeRecipeRowService;
 
     @Override
     public ResponseEntity<?> removeRecipeById(Long recipeId){
@@ -26,10 +38,17 @@ public class RemoveRecipeServiceImpl implements RemoveRecipeService {
         Recipe recipe = recipeRepository.findById(recipeId).orElseThrow(RecipeNotFoundException::new);
         Long recipeWriterId = recipe.getBoard().getWriter().getId();
 
+        // TODO: exception
         if (!Objects.equals(loginUser, recipeWriterId)){
             throw new RuntimeException("삭제 권한이 없습니다.");
         }
 
+        List<RecipeIngredient> recipeIngredients = recipeIngredientRepository.findRecipeIngredientsByRecipeId(recipeId);
+        List<RecipeRow> recipeRows = recipeRowRepository.findRecipeRowsByRecipeIdOrderById(recipeId);
+
+        // TODO: removeRecipeRow 개선 (권한 검증 생략 + recipeRow 객체로 받아서 삭제 처리)
+        recipeRows.forEach(recipeRow -> removeRecipeRowService.removeRecipeRow(recipeId, recipeRow.getId()));
+        recipeIngredientRepository.deleteAll(recipeIngredients);
         recipeRepository.delete(recipe);
         boardRepository.delete(recipe.getBoard());
 
