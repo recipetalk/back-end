f2f0e8c7087d7e9d673f80655486a6e160346e4b
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2023-05-03 23:13:46 +0900

    add: 내가 소유한 식재료 수정 권한 검증 추가 및 dto validation 추가

--- src/main/java/com/solution/recipetalk/service/ingredient/userhas/impl/EditUserHasIngredientServiceImpl.java
+++ src/main/java/com/solution/recipetalk/service/ingredient/userhas/impl/EditUserHasIngredientServiceImpl.java
@@ -3,12 +3,16 @@ package com.solution.recipetalk.service.ingredient.userhas.impl;
 import com.solution.recipetalk.domain.ingredient.userhas.entity.UserHasIngredient;
 import com.solution.recipetalk.domain.ingredient.userhas.repository.UserHasIngredientRepository;
 import com.solution.recipetalk.dto.ingredient.userhas.UserHasIngredientModifyDTO;
+import com.solution.recipetalk.exception.common.NotAuthorizedException;
 import com.solution.recipetalk.exception.ingredient.IngredientNotFoundException;
 import com.solution.recipetalk.service.ingredient.userhas.EditUserHasIngredientService;
+import com.solution.recipetalk.util.ContextHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 
 @Service
@@ -19,8 +23,12 @@ public class EditUserHasIngredientServiceImpl implements EditUserHasIngredientSe
 
     @Override
     public ResponseEntity<?> modifyUserHasIngredient(Long userHasIngredientId, UserHasIngredientModifyDTO dto) {
+        Long loginUser = ContextHolder.getUserLoginId();
         UserHasIngredient byId = userHasIngredientRepository.findById(userHasIngredientId).orElseThrow(IngredientNotFoundException::new);
 
+        if (!Objects.equals(loginUser, byId.getUser().getId())){
+            throw new NotAuthorizedException();
+        }
         byId.updateUserHasIngredient(dto);
 
         userHasIngredientRepository.save(byId);
