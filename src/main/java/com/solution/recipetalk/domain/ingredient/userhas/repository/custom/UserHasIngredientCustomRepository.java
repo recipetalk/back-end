c3f4083bc9211adeb4082c524c31a8124a93cd4e
ghdwlgns <atene1408@gmail.com>
2023-05-08 14:13:05 +0900

    fix : 동적 쿼리 동작 안하던 오류 수정

--- src/main/java/com/solution/recipetalk/domain/ingredient/userhas/repository/custom/UserHasIngredientCustomRepository.java
+++ src/main/java/com/solution/recipetalk/domain/ingredient/userhas/repository/custom/UserHasIngredientCustomRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface UserHasIngredientCustomRepository {
-    Page<UserHasIngredientResponseDTO> findAllUserIngredient(Pageable pageable, Long startId, String sortElement);
+    Page<UserHasIngredientResponseDTO> findAllUserIngredient(Pageable pageable, String sortElement);
 }
