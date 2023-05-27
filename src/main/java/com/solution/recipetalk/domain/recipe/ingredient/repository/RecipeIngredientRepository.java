a0348b3f48425b2d1fc8f7f7271c218f52cc7082
KimHyunJin <khj745700@naver.com>
2023-05-27 17:11:20 +0900

    fix : recipeIngredient 조회 isHas 안되는 것 수정

--- src/main/java/com/solution/recipetalk/domain/recipe/ingredient/repository/RecipeIngredientRepository.java
+++ src/main/java/com/solution/recipetalk/domain/recipe/ingredient/repository/RecipeIngredientRepository.java
@@ -21,8 +21,8 @@ public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredie
     @Query(value = "SELECT ri.name as name, case WHEN uhi is not null THEN true WHEN uhi2 is not null THEN true ELSE false END as isHas, ri.quantity as quantity, i.id AS ingredientId " +
             "FROM RecipeIngredient ri " +
             "LEFT JOIN Ingredient i ON ri.ingredient = i " +
-            "LEFT JOIN UserHasIngredient uhi ON uhi.user.id = :viewerId AND ri.name like '%' + uhi.name + '%' " +
-            "LEFT JOIN UserHasIngredient uhi2 ON uhi2.user.id = :viewerId AND ri.ingredient = uhi.ingredient " +
+            "LEFT JOIN UserHasIngredient uhi ON uhi.user.id = :viewerId AND ri.name = uhi.name " +
+            "LEFT JOIN UserHasIngredient uhi2 ON uhi2.user.id = :viewerId AND ri.ingredient = uhi2.ingredient " +
             "WHERE ri.recipe.id = :recipeId")
     List<RecipeIngredientResult> findRecipeIngredientByUserIdAndRecipeId(@Param("viewerId")Long userId, @Param("recipeId")Long recipeId);
 
