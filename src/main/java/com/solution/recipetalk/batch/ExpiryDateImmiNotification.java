75acea57267d0b34a1b9dd91de1636a6c7b51e53
KimHyunJin <khj745700@naver.com>
2023-06-03 07:58:19 +0900

    fix: 식재료 마감 정상적으로 동작 안됨 수정

--- src/main/java/com/solution/recipetalk/batch/ExpiryDateImmiNotification.java
+++ src/main/java/com/solution/recipetalk/batch/ExpiryDateImmiNotification.java
@@ -1,12 +1,14 @@
 package com.solution.recipetalk.batch;
 
 import com.solution.recipetalk.domain.ingredient.userhas.repository.UserHasIngredientRepository;
+import com.solution.recipetalk.domain.notification.repository.NotificationRepository;
 import com.solution.recipetalk.vo.notification.ingredient.userhas.UserHasIngredientNotificationVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,10 +16,12 @@ import java.util.List;
 @EnableScheduling
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class ExpiryDateImmiNotification {
 
     private final UserHasIngredientRepository userHasIngredientRepository;
     private final ApplicationEventPublisher eventPublisher;
+    private final NotificationRepository notificationRepository;
     //매일 오전 1시에
     @Scheduled(cron = "0 0 1 * * *")
     public void run () {
@@ -29,6 +33,7 @@ public class ExpiryDateImmiNotification {
         userHasIngredientsByExpirationDate.forEach(expiryDateImmiIngredientDTO -> {
             UserHasIngredientNotificationVO vo = new UserHasIngredientNotificationVO(expiryDateImmiIngredientDTO, term);
             eventPublisher.publishEvent(vo);
+            notificationRepository.save(vo.toEntity());
         });
     }
 }
