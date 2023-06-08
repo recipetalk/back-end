2311bd3127037d6933dd173dc76f37408645acb3
KimHyunJin <khj745700@naver.com>
2023-06-08 15:41:31 +0900

    fix: productRequest barcode 여러개 올 수 있기 때문에 @Id 수정

--- src/main/java/com/solution/recipetalk/domain/product/request/ProductRequest.java
+++ src/main/java/com/solution/recipetalk/domain/product/request/ProductRequest.java
@@ -7,13 +7,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "product_request")
+@Table(name = "product_request", indexes = {
+        @Index(name = "idx_barcode", columnList = "barcode", unique = false)
+})
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
 public class ProductRequest {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private Long barcode;
 
     @Column(name = "product_name")
