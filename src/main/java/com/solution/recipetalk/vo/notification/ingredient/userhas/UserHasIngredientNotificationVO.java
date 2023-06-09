504f112a2d9e4553bcc07c36482a208bd9e52936
KimHyunJin <khj745700@naver.com>
2023-06-10 01:03:47 +0900

    fix: 오탈자 수정

--- src/main/java/com/solution/recipetalk/vo/notification/ingredient/userhas/UserHasIngredientNotificationVO.java
+++ src/main/java/com/solution/recipetalk/vo/notification/ingredient/userhas/UserHasIngredientNotificationVO.java
@@ -18,7 +18,7 @@ public class UserHasIngredientNotificationVO implements NotificationVO {
 
     private static final String TITLE = "레시피톡";
 
-    private static final String BODY_PATTERN_WITHOUT_COUNT = "%s님, %s가 %d일 내에 소비기한 마감됩니다. 얼른 드셔주세요!";
+    private static final String BODY_PATTERN_WITHOUT_COUNT = "%s님, %s(이)가 %d일 내에 소비기한 마감됩니다. 얼른 드셔주세요!";
     private static final String BODY_PATTERN = "%s님, %s외 %d개가 %d일 내에 소비기한 마감됩니다. 얼른 드셔주세요!";
 
     public UserHasIngredientNotificationVO(UserHasIngredientRepository.ExpiryDateImmiIngredientDTO expiryDateImmiIngredient, Long term){
